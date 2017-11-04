1eb40207800d672da64a8c4af6cb3fcc41c4e006
MIKI90 <malejo35@outlook.com>
2017-11-04 16:45:17 -0600

    Singin Activity
    Se agrego Singing Activity y prototipo de layout

---
+++ Queretaro/app/src/main/java/com/stardust/android/qro/singin/SinginActivity.java
@@ -0,0 +1,15 @@
+package com.stardust.android.qro.singin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.stardust.android.qro.R;
+
+public class SinginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_singin);
+    }
+}
